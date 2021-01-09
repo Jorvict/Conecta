4b2d8fde91186f1ab59302a6c8ad2f849d8ce15b
Jorvict Piña <jorvict15@gmail.com>
2021-01-08 19:08:29 -0500

    Correcion menu sobrepuesto

--- vistas/panel_usuario/js/menu.js
+++ vistas/panel_usuario/js/menu.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector('.nav');
+const Ho = document.querySelector('.menuHo');
 const menuButton = document.querySelector('.burger-button');
 
 function ShowHideMenu(){
   if (menu.classList.contains('is-active')){
     menu.classList.remove('is-active');
+    Ho.classList.remove('initial');
   }
   else{
     menu.classList.add('is-active');
+    Ho.classList.add('initial');
   }
 }
 menuButton.addEventListener('click', ShowHideMenu)
