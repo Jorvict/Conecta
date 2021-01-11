3c666b818494409c669850df7d211e2018d534e0
Jorvict Piña <jorvict15@gmail.com>
2021-01-11 18:07:09 -0500

    Se realiza cambio de fondo páginas pedidos y ventas

--- vistas/panel_usuario/js/menu.js
+++ vistas/panel_usuario/js/menu.js
@@ -5,11 +5,9 @@ const menuButton = document.querySelector('.burger-button');
 function ShowHideMenu(){
   if (menu.classList.contains('is-active')){
     menu.classList.remove('is-active');
-    Ho.classList.remove('initial');
   }
   else{
     menu.classList.add('is-active');
-    Ho.classList.add('initial');
   }
 }
 menuButton.addEventListener('click', ShowHideMenu)
